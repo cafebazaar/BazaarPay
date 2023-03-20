91b56739c5cb209d1ed58b75687fa207335064eb
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-03-20 17:37:04 +0330

    Remove redundant BazaarPayLauncher API

--- BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/BazaarPayLauncher.kt
+++ BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/BazaarPayLauncher.kt
@@ -1,62 +1,12 @@
 package ir.cafebazaar.bazaarpay
 
-import android.app.Activity.RESULT_CANCELED
-import android.app.Activity.RESULT_OK
 import android.content.Context
-import android.content.Intent
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import ir.cafebazaar.bazaarpay.BazaarPayActivity.Companion.BAZAARPAY_ACTIVITY_ARGS
-import ir.cafebazaar.bazaarpay.arg.BazaarPayActivityArgs
 import ir.cafebazaar.bazaarpay.data.bazaar.models.ErrorModel
 import ir.cafebazaar.bazaarpay.data.payment.PaymentRepository
 import ir.cafebazaar.bazaarpay.data.payment.models.pay.PurchaseStatus
 import ir.cafebazaar.bazaarpay.extensions.fold
 import ir.cafebazaar.bazaarpay.utils.getLanguage
 import ir.cafebazaar.bazaarpay.utils.getLanguageNumber
-
-object BazaarPayLauncher {
-
-    fun launchBazaarPay(
-        context: Context,
-        checkoutToken: String,
-        phoneNumber: String? = null,
-        isDarkMode: Boolean = false,
-        isEnglish: Boolean = false,
-        activityResultLauncher: ActivityResultLauncher<Intent>
-    ) {
-        ServiceLocator.initializeConfigs(
-            checkoutToken,
-            phoneNumber,
-            isDarkMode,
-            getLanguage(isEnglish),
-            getLanguageNumber(isEnglish)
-        )
-        val bazaarPayActivityArgs = BazaarPayActivityArgs(
-            checkoutToken,
-            phoneNumber,
-            isDarkMode,
-            getLanguage(isEnglish),
-            getLanguageNumber(isEnglish)
-        )
-        Intent(context, BazaarPayActivity::class.java)
-            .also {
-                it.putExtra(BAZAARPAY_ACTIVITY_ARGS, bazaarPayActivityArgs)
-                activityResultLauncher.launch(it)
-            }
-    }
-
-    fun onResultLauncher(
-        result: ActivityResult,
-        onSuccess: () -> Unit,
-        onCancel: () -> Unit
-    ) {
-        when (result.resultCode) {
-            RESULT_OK -> onSuccess()
-            RESULT_CANCELED -> onCancel()
-        }
-    }
-}
 
 fun initSDKForAPICall(
     context: Context,
