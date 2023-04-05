ae2ff56cd0c4ec36d9a2033acfd6c959a8e24bae
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-05 18:09:06 +0430

    Add documentation of the BazaarPayLauncher

--- BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/BazaarPayLauncher.kt
+++ BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/BazaarPayLauncher.kt
@@ -7,12 +7,26 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import ir.cafebazaar.bazaarpay.arg.BazaarPayActivityArgs
 
+/**
+ * Handler object for launching Bazaar payment and parsing the result.
+ *
+ * Deprecated in favor of [StartBazaarPay].
+ */
 @Deprecated(
     message = "Use the StartBazaarPay class instead",
     level = DeprecationLevel.WARNING
 )
 object BazaarPayLauncher {
 
+    /**
+     * Launches BazaarPay flow.
+     *
+     * @property context the context in which payment happens.
+     * @property checkoutToken the unique identifier that provides essential payment information.
+     * @property isDarkMode enables *Dark-Mode* for the UI elements of the payment flow, which are in *Light-Mode* by default.
+     * @property phoneNumber the default phone number to pre-fill the login screen's input field. It uses a `null` value by default, resulting in no pre-filled input.
+     * @property activityResultLauncher an instance of the [ActivityResultLauncher] registered using the Activity Result API.
+     */
     fun launchBazaarPay(
         context: Context,
         checkoutToken: String,
@@ -37,6 +51,11 @@ object BazaarPayLauncher {
             }
     }
 
+    /**
+     * Notifies BazaarPay about the incoming [ActivityResult].
+     *
+     * After parsing the result, either [onSuccess] or [onCancel] callbacks will be executed.
+     */
     fun onResultLauncher(
         result: ActivityResult,
         onSuccess: () -> Unit,
