a26a6257d6a9b82d83de73357be9d4e83031c950
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-25 19:27:26 +0430

    Update initCheckout documentation

--- BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/BazaarPayApiWrappers.kt
+++ BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/BazaarPayApiWrappers.kt
@@ -74,14 +74,14 @@ suspend fun trace(
 }
 
 /**
- * Init the [checkoutToken] in order to initiate a purchase flow.
+ * Initiates the checkout process.
  *
- * @param context the context in which tracing happens.
+ * @param context the context in which init happens.
  * @param amount the amount of the purchase in Rials. Notice that this has some limitations which you should consider based on BazaarPay documentation.
  * @param destination the destination of the purchase which you should get it from the BazaarPay team.
  * @param serviceName the serviceName of the purchase which you should get it from the BazaarPay team.
- * @param onSuccess the callback when init checkout payment successfully offering its [InitCheckoutResult].
- * @param onFailure the callback for an unsuccessful initiating of checkout with [ErrorModel] to reason about the cause.
+ * @param onSuccess the callback when initiated checkout successfully offering its [InitCheckoutResult].
+ * @param onFailure the callback for an unsuccessful initiation of checkout with [ErrorModel] to reason about the cause.
  */
 suspend fun initCheckout(
     context: Context,
