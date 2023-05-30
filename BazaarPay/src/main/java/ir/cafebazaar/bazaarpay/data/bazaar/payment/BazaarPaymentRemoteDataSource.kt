b686c738e8a7057063f04562c9e046af0d811a10
Hamid <shr.mahmoodi@gmail.com>
2023-05-30 10:49:18 +0330

    Handle error models

--- BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/data/bazaar/payment/BazaarPaymentRemoteDataSource.kt
+++ BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/data/bazaar/payment/BazaarPaymentRemoteDataSource.kt
@@ -10,6 +10,7 @@ import ir.cafebazaar.bazaarpay.data.bazaar.payment.models.directdebit.contractcr
 import ir.cafebazaar.bazaarpay.data.bazaar.payment.models.directdebit.onboarding.DirectDebitOnBoardingDetails
 import ir.cafebazaar.bazaarpay.data.bazaar.payment.models.directdebit.onboarding.request.GetDirectDebitOnBoardingSingleRequest
 import ir.cafebazaar.bazaarpay.data.bazaar.payment.models.postpaid.activate.request.ActivatePostpaidCreditSingleRequest
+import ir.cafebazaar.bazaarpay.extensions.ServiceType
 import ir.cafebazaar.bazaarpay.extensions.safeApiCall
 import ir.cafebazaar.bazaarpay.models.GlobalDispatchers
 import ir.cafebazaar.bazaarpay.utils.Either
@@ -29,7 +30,7 @@ internal class BazaarPaymentRemoteDataSource {
 
     suspend fun getDirectDebitOnBoarding(): Either<DirectDebitOnBoardingDetails> {
         return withContext(globalDispatchers.iO) {
-            return@withContext safeApiCall {
+            return@withContext safeApiCall(serviceType = ServiceType.BAZAARPAY) {
                 bazaarService.getDirectDebitOnBoarding(
                     GetDirectDebitOnBoardingSingleRequest(checkoutToken = checkoutToken)
                 ).toDirectDebitOnBoardingDetails()
@@ -42,7 +43,7 @@ internal class BazaarPaymentRemoteDataSource {
         nationalId: String
     ): Either<ContractCreation> {
         return withContext(globalDispatchers.iO) {
-            return@withContext safeApiCall {
+            return@withContext safeApiCall(serviceType = ServiceType.BAZAARPAY) {
                 bazaarService.getCreateContractUrl(
                     GetDirectDebitContractCreationUrlSingleRequest(
                         bankCode = bankCode,
@@ -58,7 +59,7 @@ internal class BazaarPaymentRemoteDataSource {
 
     suspend fun getAvailableBanks(): Either<AvailableBanks> {
         return withContext(globalDispatchers.iO) {
-            return@withContext safeApiCall {
+            return@withContext safeApiCall(serviceType = ServiceType.BAZAARPAY) {
                 bazaarService.getAvailableBanks(
                     GetAvailableBanksSingleRequestDto(checkoutToken = checkoutToken)
                 ).toAvailableBanks()
@@ -68,7 +69,7 @@ internal class BazaarPaymentRemoteDataSource {
 
     suspend fun activatePostPaid(): Either<Unit> {
         return withContext(globalDispatchers.iO) {
-            return@withContext safeApiCall<Unit> {
+            return@withContext safeApiCall<Unit>(serviceType = ServiceType.BAZAARPAY) {
                 bazaarService.activatePostPaid(
                     ActivatePostpaidCreditSingleRequest(checkoutToken = checkoutToken)
                 )
