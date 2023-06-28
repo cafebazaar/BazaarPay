a988f48971405fe7572657e02164c88d55335a43
Hamid <shr.mahmoodi@gmail.com>
2023-06-28 12:53:59 +0330

    Fix OnBoardingViewHolder issue in landscape

--- BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/screens/payment/directdebitonboarding/DirectDebitOnBoardingViewHolder.kt
+++ BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/screens/payment/directdebitonboarding/DirectDebitOnBoardingViewHolder.kt
@@ -1,12 +1,8 @@
 package ir.cafebazaar.bazaarpay.screens.payment.directdebitonboarding
 
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import ir.cafebazaar.bazaarpay.R
 import ir.cafebazaar.bazaarpay.data.bazaar.payment.models.directdebit.onboarding.OnBoardingItem
-import ir.cafebazaar.bazaarpay.data.payment.models.getpaymentmethods.Option
 import ir.cafebazaar.bazaarpay.databinding.ItemDirectDebitOnboardingBinding
-import ir.cafebazaar.bazaarpay.databinding.ItemPaymentDynamicCreditBinding
 import ir.cafebazaar.bazaarpay.utils.imageloader.BazaarPayImageLoader
 
 internal class DirectDebitOnBoardingViewHolder(
