36ee2565ea555a4908f582e6929a7264e424fe90
Hamid <shr.mahmoodi@gmail.com>
2023-07-03 15:49:08 +0330

    Add SampleDirectPayContractActivity to SamplePaymentActivity

---
+++ app/src/main/java/ir/cafebazaar/bazaarpay/sample/SampleDirectPayContractActivity.kt
@@ -0,0 +1,12 @@
+package ir.cafebazaar.bazaarpay.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SampleDirectPayContractActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample_direct_pay_contract_activity)
+    }
+}
