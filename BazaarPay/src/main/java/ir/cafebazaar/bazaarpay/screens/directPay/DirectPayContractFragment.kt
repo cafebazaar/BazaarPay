05096888b6cfb7db1e418e9651aee0d98269525e
Hamid <shr.mahmoodi@gmail.com>
2023-07-05 14:01:16 +0330

    Handle backPress in DirectPayContractFragment

--- BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/screens/directPay/DirectPayContractFragment.kt
+++ BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/screens/directPay/DirectPayContractFragment.kt
@@ -1,10 +1,13 @@
 package ir.cafebazaar.bazaarpay.screens.directPay
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
+import ir.cafebazaar.bazaarpay.FinishCallbacks
 import ir.cafebazaar.bazaarpay.databinding.FragmentDirectPayContractBinding
 import ir.cafebazaar.bazaarpay.utils.bindWithRTLSupport
 
@@ -13,6 +16,16 @@ class DirectPayContractFragment : Fragment() {
     private var _binding: FragmentDirectPayContractBinding? = null
     private val binding: FragmentDirectPayContractBinding
         get() = requireNotNull(_binding)
+
+    private var finishCallbacks: FinishCallbacks? = null
+
+    override fun onAttach(context: Context) {
+        finishCallbacks = context as? FinishCallbacks
+            ?: throw IllegalStateException(
+                "this activity must implement FinishPaymentCallbacks"
+            )
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +37,24 @@ class DirectPayContractFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            handleBackPress()
+        }
+    }
+
+    private fun handleBackPress() {
+        finishCallbacks?.onCanceled()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onDetach() {
+        finishCallbacks = null
+        super.onDetach()
     }
 }
