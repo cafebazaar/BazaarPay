82baedb3c7501f5a3d433cd0d3d9f590b9252f92
Hamid <shr.mahmoodi@gmail.com>
2023-07-19 15:11:04 +0330

    Add service name to route instead baseURL

--- BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/data/payment/api/PaymentService.kt
+++ BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/data/payment/api/PaymentService.kt
@@ -18,35 +18,35 @@ import retrofit2.http.Query
 
 internal interface PaymentService {
 
-    @POST("get-payment-methods/")
+    @POST("pardakht/badje/v1/get-payment-methods/")
     suspend fun getPaymentMethods(
         @Body getPaymentMethodsRequest: GetPaymentMethodsRequest,
         @Query(PAY_ENDPOINT_LANG) lang: String
     ): PaymentMethodsInfoDto
 
-    @GET("merchant-info")
+    @GET("pardakht/badje/v1/merchant-info")
     suspend fun getMerchantInfo(
         @Query(CHECKOUT_TOKEN_LABEL) checkoutLabel: String,
         @Query(PAY_ENDPOINT_LANG) lang: String
     ): MerchantInfoDto
 
-    @POST("pay/")
+    @POST("pardakht/badje/v1/pay/")
     suspend fun pay(
         @Body payRequest: PayRequest,
         @Query(PAY_ENDPOINT_LANG) lang: String
     ): PayResponse
 
-    @POST("commit/")
+    @POST("pardakht/badje/v1/commit/")
     suspend fun commit(
         @Body commitRequest: CommitRequest
     ): ResponseBody
 
-    @POST("trace/")
+    @POST("pardakht/badje/v1/trace/")
     suspend fun trace(
         @Body traceRequest: TraceRequest
     ): TraceResponse
 
-    @POST("checkout/init/")
+    @POST("pardakht/badje/v1/checkout/init/")
     suspend fun initCheckout(
         @Body initCheckoutRequest: InitCheckoutRequest
     ): InitCheckoutResponse
