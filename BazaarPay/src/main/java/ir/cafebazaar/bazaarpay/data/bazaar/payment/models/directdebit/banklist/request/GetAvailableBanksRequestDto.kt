09939ad0a2ce55fe67aa3fbdeaec3c5818edf5f8
Hamid <shr.mahmoodi@gmail.com>
2023-07-24 10:45:10 +0330

    Remove redundant requests

--- BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/data/bazaar/payment/models/directdebit/banklist/request/GetAvailableBanksRequestDto.kt
+++
@@ -1,7 +0,0 @@
-package ir.cafebazaar.bazaarpay.data.bazaar.payment.models.directdebit.banklist.request
-
-import com.google.gson.annotations.SerializedName
-
-internal class GetAvailableBanksSingleRequestDto(
-    @SerializedName("checkout_token") val checkoutToken: String
-)
