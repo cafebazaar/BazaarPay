56c10e23914621f4ee286e1d52b8616a175cab6b
Hamid <shr.mahmoodi@gmail.com>
2023-07-28 12:41:19 +0330

    Implement getBalance in sampleApp

--- app/src/main/java/ir/cafebazaar/bazaarpay/sample/balance/BalanceSampleActivity.kt
+++ app/src/main/java/ir/cafebazaar/bazaarpay/sample/balance/BalanceSampleActivity.kt
@@ -3,9 +3,14 @@ package ir.cafebazaar.bazaarpay.sample.balance
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import ir.cafebazaar.bazaarpay.getBazaarPayBalance
 import ir.cafebazaar.bazaarpay.login.BazaarPayLoginOptions
 import ir.cafebazaar.bazaarpay.login.StartLogin
+import ir.cafebazaar.bazaarpay.sample.R
 import ir.cafebazaar.bazaarpay.sample.databinding.ActivityBalanceSampleBinding
+import kotlinx.coroutines.launch
 
 class BalanceSampleActivity : AppCompatActivity() {
 
@@ -15,9 +20,9 @@ class BalanceSampleActivity : AppCompatActivity() {
         StartLogin()
     ) { isSuccessful ->
         if (isSuccessful) {
-            Toast.makeText(this, "لاگین موفقیت آمیز بود", Toast.LENGTH_SHORT).show()
+            loadUserBalance()
         } else {
-            Toast.makeText(this, "لاگین ناموفق بود", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.failure_login), Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -25,7 +30,30 @@ class BalanceSampleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityBalanceSampleBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        startLogin()
+        loadUserBalance()
+        binding.needLoginButton.setOnClickListener { startLogin() }
+    }
+
+    private fun loadUserBalance() = lifecycleScope.launch {
+        getBazaarPayBalance(
+            this@BalanceSampleActivity,
+            onSuccess = {
+                updateVisibilityBasedOnLoginState(false)
+                binding.txtBalance.text = it.humanReadableAmount
+            },
+            onFailure = {
+                updateVisibilityBasedOnLoginState(false)
+                binding.txtBalance.text = it.message
+            },
+            onLoginNeeded = {
+                updateVisibilityBasedOnLoginState(true)
+            }
+        )
+    }
+
+    private fun updateVisibilityBasedOnLoginState(isNeeded: Boolean) {
+        binding.balanceGroup.isVisible = isNeeded.not()
+        binding.needLoginButton.isVisible = isNeeded
     }
 
     private fun startLogin(phone: String? = null) {
