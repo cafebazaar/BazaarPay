ebb8d8150cdf3f17b5ca15f08b1023008f90a376
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2023-10-02 16:37:56 +0330

    remove unused import

--- BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/data/bazaar/account/UserInfoService.kt
+++ BazaarPay/src/main/java/ir/cafebazaar/bazaarpay/data/bazaar/account/UserInfoService.kt
@@ -2,10 +2,9 @@ package ir.cafebazaar.bazaarpay.data.bazaar.account
 
 import ir.cafebazaar.bazaarpay.data.bazaar.account.models.userinfo.AutoLoginUserInfoReplyDto
 import retrofit2.http.GET
-import retrofit2.http.POST
 
 internal interface UserInfoService {
 
-    @GET("pardakht/badje/v1/user/info//")
+    @GET("pardakht/badje/v1/user/info/")
     suspend fun getUserInfoRequest(): AutoLoginUserInfoReplyDto
 }
